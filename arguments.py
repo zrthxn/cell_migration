7e2a0cc5cc1e402ac9be13fa3e56e495dbddcf92
Alisamar Husain <zrthxn@gmail.com>
2024-11-07 02:46:50 +0100

    Fix training

--- arguments.py
+++ arguments.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from pathlib import Path
 
 
 class TrainArguments(ArgumentParser):
@@ -9,16 +10,16 @@ class TrainArguments(ArgumentParser):
         self.add_argument("--series", action="store", nargs="*", required=True)
         self.add_argument("--limit", type=int, default=None)
         self.add_argument("--train_val_split", type=float, default=0.9)
-        self.add_argument("--plot_dir", type=str, default=".")
-        self.add_argument("--save_to", type=str, default=None)
+        self.add_argument("--plot_dir", type=Path, default=".")
+        self.add_argument("--save_to", type=Path, default=None)
 
 
 class ValidationArguments(ArgumentParser):
     def __init__(self) -> None:
         super().__init__()
         self.add_argument("--network", choices=["sequencenet", "transformer"], required=True)
-        self.add_argument("--checkpoint", type=str, required=True)
+        self.add_argument("--checkpoint", type=Path, required=True)
         self.add_argument("--parameters", type=str, required=True)
         self.add_argument("--series", action="store", nargs="*", required=True)
         self.add_argument("--limit", type=int, default=None)
-        self.add_argument("--plot_dir", type=str, default=".")
+        self.add_argument("--plot_dir", type=Path, default=".")
--- training.py
+++ training.py
@@ -20,12 +20,10 @@ params, series = load_dataset(args.parameters, args.series, args.limit)
 prior = Prior(prior_fun=lambda: params[randint(0, len(params) - 1)], param_names=["P1", "P2", "P3"])
 
 prior_mean, prior_std = prior.estimate_means_and_stds()
-prior.plot_prior2d().savefig(Path(args.plot_dir) / "training_prior.png")
-print(f"Prior(mean={prior_mean}, std={prior_std})")
+prior.plot_prior2d().savefig(args.plot_dir / "training_prior.png")
 
 series_mean = np.mean(series, axis=2)[:, :, np.newaxis]
 series_std = np.std(series, axis=2)[:, :, np.newaxis]
-print(f"Series(mean={series_mean}, std={series_std})")
 
 # Normalize Parameters and series
 params = (params - prior_mean)  / prior_std
@@ -48,17 +46,14 @@ else:
 
 trainer = Trainer(amortizer=amortizer, configurator=amortizer.configurator, memory=True, checkpoint_path=args.save_to)
 history = trainer.train_offline(training_data, 
-    epochs=100, 
+    epochs=2, 
     batch_size=64, 
     early_stopping=True, 
     validation_sims=validation_data, 
     save_checkpoint=True)
 
 plot_losses(np.log(history["train_losses"]), np.log(history["val_losses"]), moving_average=True)\
-    .savefig(Path(args.plot_dir) / "training_losses.png")
-
-trainer.diagnose_latent2d()\
-    .savefig(Path(args.plot_dir) / "training_diagnose_latent2d.png")
+    .savefig(args.plot_dir / "training_losses.png")
 
 # Generate posterior draws for all simulations
 validation_sims = trainer.configurator(validation_data)
@@ -66,15 +61,15 @@ post_samples = amortizer.sample(validation_sims, n_samples=100)
 
 # Create ECDF plot
 plot_sbc_ecdf(post_samples, validation_sims["parameters"], param_names=prior.param_names)\
-    .savefig(Path(args.plot_dir) / "training_sbc_ecdf.png")
+    .savefig(args.plot_dir / "training_sbc_ecdf.png")
 plot_sbc_ecdf(post_samples, validation_sims["parameters"], param_names=prior.param_names, stacked=True, difference=True)\
-    .savefig(Path(args.plot_dir) / "training_sbc_ecdf_stacked.png")
+    .savefig(args.plot_dir / "training_sbc_ecdf_stacked.png")
 plot_sbc_histograms(post_samples, validation_sims["parameters"], param_names=prior.param_names)\
-    .savefig(Path(args.plot_dir) / "training_sbc_ecdf_histogram.png")
+    .savefig(args.plot_dir / "training_sbc_ecdf_histogram.png")
 
 # TODO:TODO: De-normalize validation data using series mean/std and prior mean/std
 # TODO:TODO: Plot both norm and de-norm values
 # TODO:TODO: density contour plot instead of points
 # TODO: Recovery with cell and fish ids, abuse recovery plot maybe to show uncertainty in each param se
 plot_recovery(post_samples, validation_sims["parameters"], param_names=prior.param_names)\
-    .savefig(Path(args.plot_dir) / "training_recovery.png")
+    .savefig(args.plot_dir / "training_recovery.png")
